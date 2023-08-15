349c1bd3581286bd54e853bf6cfc7bd92928c096
Jesús Sanz <jesussanz77@gmail.com>
2023-08-15 13:35:51 +0200

    initial app
    [MAJOR] - initial commit with the base project

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Hello(){
+    return (
+        <div className="text-xl">Esta aplicación mejorará la vida de nuestros animales</div>
+    )
+}
+
+export default Hello
