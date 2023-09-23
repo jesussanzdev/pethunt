0997ac1c0cc96d7ab81f9c536fc60dafeda98dc1
Jesús Sanz <jesussanz77@gmail.com>
2023-09-23 22:46:44 +0200

    margenes en el contenedor

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <body>
-        <div className="container mx-auto grid pt-4 pm-4">
+        <div className="container mx-auto grid pt-8 pb-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
 
             <div className="flex justify-center">
